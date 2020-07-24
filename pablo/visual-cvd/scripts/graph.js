1cc619694a75f43bf51dfc0152da892643b0b0ff
Pablo Solano <pasolano@email.wm.edu>
2020-07-23 22:40:56 -0700

    added upload csv

---
+++ pablo/visual-cvd/scripts/graph.js
@@ -0,0 +1,50 @@
+var app = angular.module("graphApp", []);
+app.controller("graphCtrl", function($scope) {
+  
+  // saving csv
+  document.getElementById('inputfile')
+  .addEventListener('change', function() { 
+
+    var fr=new FileReader(); 
+    fr.onload=function(){
+      $scope.csvObjs = $.csv.toObjects(fr.result);
+    } 
+    
+    fr.readAsText(this.files[0]);
+  });
+
+  $scope.compare = function () {
+    trace1 = {
+      type: 'scatter',
+      x: [1, 2, 3, 4],
+      y: [10, 15, 13, 17],
+      mode: 'lines',
+      name: 'Red',
+      line: {
+        color: 'rgb(219, 64, 82)',
+        width: 3
+      }
+    };
+    
+    trace2 = {
+      type: 'scatter',
+      x: [1, 2, 3, 4],
+      y: [12, 9, 15, 12],
+      mode: 'lines',
+      name: 'Blue',
+      line: {
+        color: 'rgb(55, 128, 191)',
+        width: 1
+      }
+    };
+    
+    var layout = {
+      width: 500,
+      height: 500
+    };
+    
+    var data = [trace1, trace2];
+    
+    Plotly.newPlot('graph', data, layout);
+  };
+});
