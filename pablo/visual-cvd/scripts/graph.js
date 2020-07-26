403aead672988e440d95d5cf815a636d24a79655
Pablo Solano <pasolano@email.wm.edu>
2020-07-25 23:54:10 -0700

    everything but patient color

--- pablo/visual-cvd/scripts/graph.js
+++ pablo/visual-cvd/scripts/graph.js
@@ -1,14 +1,21 @@
-var app = angular.module("graphApp", []);
+var app = angular.module("graphApp", ['ui.select', 'ngSanitize']);
 app.controller("graphCtrl", function($scope) {
   
+  $scope.csvObjs = null;
+  $scope.columns = {
+    columnName: null,
+    availableOptions: []
+  }
+
   // saving csv
+  // runs on page load
   document.getElementById('inputfile')
   .addEventListener('change', function() { 
 
     var fr=new FileReader(); 
     fr.onload=function(){
       $scope.csvObjs = $.csv.toObjects(fr.result);
-      $scope.columns = Object.keys($scope.csvObjs[0]);
+      $scope.columns.availableOptions = Object.keys($scope.csvObjs[0]);
       $scope.$apply();
     } 
     
@@ -16,37 +23,61 @@ app.controller("graphCtrl", function($scope) {
   });
 
   $scope.compare = function () {
-    trace1 = {
-      type: 'scatter',
-      x: [1, 2, 3, 4],
-      y: [10, 15, 13, 17],
-      mode: 'lines',
-      name: 'Red',
-      line: {
-        color: 'rgb(219, 64, 82)',
-        width: 3
+
+    // collect temporal data by ID
+    lines = {};
+    for (var i = 0; i < $scope.csvObjs.length; i++) {
+      let obj = $scope.csvObjs[i];
+      if (!(obj.ID in lines)) {
+        lines[obj.ID] = [obj.Label];
       }
-    };
-    
-    trace2 = {
-      type: 'scatter',
-      x: [1, 2, 3, 4],
-      y: [12, 9, 15, 12],
-      mode: 'lines',
-      name: 'Blue',
-      line: {
-        color: 'rgb(55, 128, 191)',
-        width: 1
+      lines[obj.ID].push(obj[$scope.columns.columnName]);
+    }
+
+    // make graph
+    lines = Object.entries(lines);
+    var red = 'rgb(219, 64, 82)';
+    var blue = 'rgb(55, 128, 191)';
+    var data = [];
+    for (var i = 0; i < lines.length; i++) {
+      if (lines[i][1][0] == 0) {
+        var lineColor = blue;
+      } else {
+        var lineColor = red;
       }
+      lines[i][1].shift();
+      let obj = {
+        type: 'scatter',
+        x: [0, 3, 24],
+        y: lines[i][1],
+        mode: 'lines',
+        name: lines[i][0],
+        line: {
+          color: lineColor,
+          width: 1
+        }
+      }
+      data.push(obj);
     };
-    
+  
+    let obj = {
+      type: 'scatter',
+      x: [0, 3, 24],
+      y: [$scope.month0, $scope.month3, $scope.month24],
+      mode: 'lines',
+      name: 'patient',
+      line: {
+        color: 'rgb(219, 64, 82)', //temp
+        width: 5
+      }
+    }
+    data.push(obj);
+
     var layout = {
       width: 500,
       height: 500
     };
-    
-    var data = [trace1, trace2];
-    
+
     Plotly.newPlot('graph', data, layout);
-  };
+  }
 });
--- pablo/visual-cvd/scripts/select2.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-    $('.csv-options').select2({ width: '100%' });
-});
